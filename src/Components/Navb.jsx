5a27780e7e732893c0691975a9fa43bb3c2001a8
alka <alkar1846@mail.com>
2021-07-25 17:19:50 +0530

    commit

--- src/Components/Navb.jsx
+++ src/Components/Navb.jsx
@@ -13,13 +13,13 @@ function Navb() {
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ms-auto">
-                            <Nav.Link href="https://arshali471.github.io/rexon/#/" className='p-3'>Home</Nav.Link>
-                            <Nav.Link href="https://arshali471.github.io/rexon/#/about" className='p-3'>About</Nav.Link>
-                            <Nav.Link href="https://arshali471.github.io/rexon/#/gallery" className='p-3'>Our gallery</Nav.Link>
-                            <Nav.Link href="https://arshali471.github.io/rexon/#/services" className='p-3'>Services</Nav.Link>
+                            <Nav.Link href="/rexon/" className='p-3'>Home</Nav.Link>
+                            <Nav.Link href="/rexon/about" className='p-3'>About</Nav.Link>
+                            <Nav.Link href="/rexon/gallery" className='p-3'>Our gallery</Nav.Link>
+                            <Nav.Link href="/rexon/services" className='p-3'>Services</Nav.Link>
                            
-                            <Nav.Link className='p-3' exact activeClassName="active_class" href='https://arshali471.github.io/rexon/#/career'>Career</Nav.Link>
-                            <Nav.Link className='p-3' exact activeClassName="active_class" href='https://arshali471.github.io/rexon/#/contact'>Contact Us</Nav.Link>
+                            <Nav.Link className='p-3' exact activeClassName="active_class" href='/rexon/career'>Career</Nav.Link>
+                            <Nav.Link className='p-3' exact activeClassName="active_class" href='/rexon/contact'>Contact Us</Nav.Link>
                         
                         </Nav>
                     </Navbar.Collapse>
